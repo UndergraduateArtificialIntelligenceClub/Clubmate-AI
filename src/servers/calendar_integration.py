0e8b74455a4c6a09145186a8738d280708381140
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2025-11-22 17:38:40 -0700

    feat: add func to get gcal service

--- src/servers/calendar_integration.py
+++ src/servers/calendar_integration.py
@@ -1,6 +1,19 @@
 from fastmcp import FastMCP
 
-# TODO: Add documentation
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+import os
+
+mcp = FastMCP("Google Calendar MCP")
+
+# SCOPES define what we can do with the Google Calendar API
+SCOPES = [
+    'https://www.googleapis.com/auth/calendar.readonly',
+    'https://www.googleapis.com/auth/calendar.events',
+]
 
 def schedule_meeting():
     pass
@@ -17,8 +30,34 @@ def add_invites():
 def check_availability():
     pass
 
+@mcp.tool()
 def get_meetings():
     pass
 
 def update_meeting_details():
     pass
+
+
+def get_service():
+    """
+    Handles Google Authentication flow and returns a service object for Google Calendar API
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    
+    # If there are no (valid) credentials available, let the user log in
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            # This opens your browser to ask for permission
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return build('calendar', 'v3', credentials=creds)
